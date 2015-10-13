c5947c1defff17e6555226b00b6248f39ec587b6
Taranjeet <reachtotj@gmail.com>
2015-10-13 23:10:31 +0530

    trending developes support added

--- githubtrending/helpers.py
+++ githubtrending/helpers.py
@@ -9,11 +9,16 @@ import click
 import os
 import textwrap
 
-
 URL = 'http://github-trending.appspot.com'
 
 USER_REQUEST = {
-	'ALL'		: 'TRENDINGREPO'
+	'REPO'		: 'TRENDINGREPO',
+	'DEV'		: 'TRENDINGDEV',
+}
+
+FUNCTION_LIST = {
+	'REPO'		: 'write_console_repo',
+	'DEV'		: 'write_console_dev',
 }
 
 def parse_page():
@@ -29,9 +34,13 @@ def parse_page():
 		return json.loads(r.text)
 	return None
 
-def get_print_size():
+def get_console_size():
+
+	return map(int,os.popen('stty size', 'r').read().split())
+
+def get_print_size_repo():
 	
-	R,C =  map(int,os.popen('stty size', 'r').read().split())
+	R,C =  get_console_size()
 
 	idx = 3
 	name = 21
@@ -64,7 +73,28 @@ def get_print_size():
 
 	return type('Enum',(),size)
 
-def get_color_code():
+def get_print_size_dev():
+
+	R,C = get_console_size()
+
+	idx = 3
+	C-=idx
+	name = int(C/3)
+	desc = C - name
+
+	size = dict({
+		"IDX"		: -idx,
+		"NAME"		: -name,
+		"DESC"		: -desc
+		})
+
+	return type('Enum',(),size)
+
+
+
+def get_color_code_repo():
+
+	''' color code for the trending repositories'''
 
 	color = dict({
 
@@ -78,18 +108,26 @@ def get_color_code():
 
 	return type('Enum',(),color)
 
+def get_color_code_dev():
 
+	''' color code for the trending developers'''
 
-def base_data(lang):
-	repos =  parse_page()
-	write_console(repos[USER_REQUEST[lang]])
+	color = dict({
 
-def write_console(repos):
+		"IDX" 		: "white",
+		"NAME"		: "yellow",
+		"DESC"		: "blue"
+
+		})
+
+	return type('Enum',(),color)
+
+def write_console_repo(repos):
 	
 	# NO NAME LANGUAGES STARS DESCRIPTION
 
-	SIZE = get_print_size()
-	COLOR = get_color_code()
+	SIZE = get_print_size_repo()
+	COLOR = get_color_code_repo()
 	click.echo()
 	click.secho("%*s" % (SIZE.IDX,'#'), nl=False, bold=True)
 	click.secho("%*s" % (SIZE.NAME,"USER/REPO"), nl=False, bold=True)
@@ -105,7 +143,30 @@ def write_console(repos):
 		click.secho("%*s" % (SIZE.STARS,eachRepo['STAR']), nl=False, bold=True,fg=COLOR.STARS)
 		click.secho("%*s" % (SIZE.DESC,eachRepo['DESC']), bold=True,fg=COLOR.DESC)
 
+def write_console_dev(devs):
+	
+	# NO NAME DESC
+
+	SIZE = get_print_size_dev()
+	COLOR =  get_color_code_dev()
+
+	click.echo()
+	click.secho("%*s" % (SIZE.IDX,'#'), nl=False, bold=True)
+	click.secho("%*s" % (SIZE.NAME,"USER/REPO"), nl=False, bold=True)
+	click.secho("%*s" % (SIZE.DESC,"DESCRIPTION"), nl=False, bold=True)
+
+	for idx,eachDev in enumerate(devs):
+		click.echo()
+		click.secho("%*s" % (SIZE.IDX,str(idx+1)), nl=False, bold=True,fg=COLOR.IDX)
+		click.secho("%*s" % (SIZE.NAME,eachDev['NAME']), nl=False, bold=True,fg=COLOR.NAME)
+		click.secho("%*s" % (SIZE.DESC,eachDev['DESC']), bold=True,fg=COLOR.DESC)
+
+
+def base_data(lang):
+	result =  parse_page()
+	eval(FUNCTION_LIST[lang]+'({0})'.format(result[USER_REQUEST[lang]]))
+	# write_console_repo(repos[USER_REQUEST[lang]])
 
 if __name__ == '__main__':
-	base_data('ALL')
+	base_data('REPO')
 
--- githubtrending/trending.py
+++ githubtrending/trending.py
@@ -4,15 +4,25 @@ from .helpers import base_data
 
 
 @click.command()
-@click.option('--lang', help="Specify the language here.",default='ALL')
-
-def cli(lang):
+@click.option('--repo', '-r', is_flag=True, help="Lists the trending repositories.")
+@click.option('--dev', '-d', is_flag=True, help="Lists the trending developers.")
+def cli(repo,dev):
 	'''
 	A command line utility to see the trending repositories and developers on Github
 	'''
-
-	base_data(lang.upper())
-
+	try:
+		if repo:
+			base_data('REPO')
+			return
+		if dev:
+			base_data('DEV')
+			return
+		# if the user does not passes any argument then list the trending repo
+		if not(repo and dev):
+			base_data('REPO')
+			return
+	except Exception as e:
+		click.secho(e.message, fg="red", bold=True)
 
 if __name__ == '__main__':
 	cli()
