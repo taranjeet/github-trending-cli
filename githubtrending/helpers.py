e1668a0838a7afd0cab7377df7b7ccf2d215cc51
Taranjeet <reachtotj@gmail.com>
2015-10-15 22:15:32 +0530

    a probable pretty output, code needs to be cleaned

--- githubtrending/helpers.py
+++ githubtrending/helpers.py
@@ -9,7 +9,7 @@ import click
 import os
 import textwrap
 
-URL = 'http://github-trending.appspot.com'
+URL = 'http://localhost:8080/'
 
 USER_REQUEST = {
 	'REPO'		: 'TRENDINGREPO',
@@ -20,6 +20,9 @@ FUNCTION_LIST = {
 	'REPO'		: 'write_console_repo',
 	'DEV'		: 'write_console_dev',
 }
+
+def L(s):
+	return len(s)
 
 def parse_page():
 
@@ -38,9 +41,119 @@ def get_console_size():
 
 	return map(int,os.popen('stty size', 'r').read().split())
 
-def get_print_size_repo():
+
+def get_col_size(data):
+
+
+	name, lang, star = [0]*3
+	for eachR in data:
+
+		name = max(L(eachR.get("NAME","")),name)
+		lang = max(L(eachR.get("LANG","")),lang)
+		star = max(L(eachR.get("STAR","")),star)
+
+	return {
+		"NAME"	: name+1,
+		"LANG"	: lang+1,
+		"STAR"	: star+1
+	}
+
+
+def print_each_line(sizeDict):
+	pass
+
+
+def get_print_size_repo(data):
 	
 	R,C =  get_console_size()
+
+	printSize = get_col_size(data)
+	COLOR = get_color_code_repo()
+
+	idx = 3
+	name = printSize["NAME"]
+	lang = printSize["LANG"]
+	star = printSize["STAR"]
+
+
+	excludingDescSum = idx + name + lang + star
+
+	desc = C - excludingDescSum
+
+	printSize["NAME"] = -name
+	printSize["LANG"] = -lang
+	printSize["IDX"]  = -idx
+	printSize["STAR"] = -star
+	printSize["DESC"] = desc
+	click.echo()
+	click.secho("%*s" % (printSize["IDX"],'#'), nl=False, bold=True)
+	click.secho("%*s" % (printSize["NAME"],"USER/REPO"), nl=False, bold=True)
+	click.secho("%*s" % (printSize["LANG"],"LANG"), nl=False, bold=True)
+	click.secho("%*s" % (printSize["STAR"],"STAR"), nl=False, bold=True)
+	click.secho("%*s" % (-printSize["DESC"],"DESCRIPTION"), nl=False, bold=True)
+
+	# dummy = data[0]
+	# each_row = '1'.ljust(printSize["IDX"]) + dummy["NAME"].ljust(printSize["NAME"]) + dummy["LANG"].ljust(printSize["LANG"]) + \
+	# dummy["STAR"].ljust(printSize["STAR"])
+	# print (each_row)
+	for idx,eachRepo in enumerate(data):
+		click.echo()
+		click.secho("%*s" % (printSize["IDX"],str(idx+1)), nl=False, bold=True,fg=COLOR.IDX)
+		click.secho("%*s" % (printSize["NAME"],eachRepo['NAME']), nl=False, bold=True,fg=COLOR.NAME)
+		click.secho("%*s" % (printSize["LANG"],eachRepo['LANG']), nl=False, bold=True,fg=COLOR.LANG)
+		click.secho("%*s" % (printSize["STAR"],eachRepo['STAR']), nl=False, bold=True,fg=COLOR.STARS)
+
+		text =  eachRepo["DESC"]
+
+		i = 0
+		descSize = printSize["DESC"]
+		# print (text,descSize)
+
+		if len(text) < descSize:
+			click.secho("%*s" % (-descSize,text), bold=True,fg=COLOR.DESC)
+		else:
+			L =len(text)
+
+			while i<L:
+
+				# if len(text)<descSize:
+				# 	click.secho("%*s" % (-descSize,text), bold=True,fg=COLOR.DESC, nl=False)
+				# 	i+=len(text)
+				# else:	
+
+
+				# if text[descSize+1] == ' ': 
+				# 	optSize = descSize
+				# else:
+				# 	firstSpace = text[:descSize].find(' ')
+				# 	if firstSpace == -1:
+				# 		firstSpace = descSize
+				# 	optSize = firstSpace-1
+				# if i == 0:
+				# 	click.secho("%*s" % (-optSize,text[:optSize]), bold=True,fg=COLOR.DESC)
+				# else:
+				# 	click.secho("%*s" % (C,text[:optSize]), bold=True,fg=COLOR.DESC)
+				# text = text[optSize+1:]
+				# i+=(optSize+1)
+				# print descSize characters
+				# print (text[descSize] == '')
+				if i == 0:
+					click.secho("%*s" % (-descSize,text[:descSize]), bold=True,fg=COLOR.DESC, nl=False)
+				else:
+					click.secho("%*s" % (excludingDescSum,' '), bold=True,fg=COLOR.DESC, nl=False)
+					click.secho("%*s" % (-C,text[:descSize]), bold=True,fg=COLOR.DESC)
+
+				text = text[descSize:]
+
+				i+=(descSize+1)
+				# print 
+
+
+
+
+
+
+	# print_each_line(printSize,data)
 
 	idx = 3
 	name = 21
@@ -126,22 +239,26 @@ def write_console_repo(repos):
 	
 	# NO NAME LANGUAGES STARS DESCRIPTION
 
-	SIZE = get_print_size_repo()
-	COLOR = get_color_code_repo()
-	click.echo()
-	click.secho("%*s" % (SIZE.IDX,'#'), nl=False, bold=True)
-	click.secho("%*s" % (SIZE.NAME,"USER/REPO"), nl=False, bold=True)
-	click.secho("%*s" % (SIZE.LANG,"LANG"), nl=False, bold=True)
-	click.secho("%*s" % (SIZE.STARS,"STAR"), nl=False, bold=True)
-	click.secho("%*s" % (SIZE.DESC,"DESCRIPTION"), nl=False, bold=True)
+	print (get_col_size(repos))
+	get_print_size_repo(repos)
 
-	for idx,eachRepo in enumerate(repos):
-		click.echo()
-		click.secho("%*s" % (SIZE.IDX,str(idx+1)), nl=False, bold=True,fg=COLOR.IDX)
-		click.secho("%*s" % (SIZE.NAME,eachRepo['NAME']), nl=False, bold=True,fg=COLOR.NAME)
-		click.secho("%*s" % (SIZE.LANG,eachRepo['LANG']), nl=False, bold=True,fg=COLOR.LANG)
-		click.secho("%*s" % (SIZE.STARS,eachRepo['STAR']), nl=False, bold=True,fg=COLOR.STARS)
-		click.secho("%*s" % (SIZE.DESC,eachRepo['DESC']), bold=True,fg=COLOR.DESC)
+
+	# SIZE = get_print_size_repo(repos)
+	# COLOR = get_color_code_repo()
+	# click.echo()
+	# click.secho("%*s" % (SIZE.IDX,'#'), nl=False, bold=True)
+	# click.secho("%*s" % (SIZE.NAME,"USER/REPO"), nl=False, bold=True)
+	# click.secho("%*s" % (SIZE.LANG,"LANG"), nl=False, bold=True)
+	# click.secho("%*s" % (SIZE.STARS,"STAR"), nl=False, bold=True)
+	# click.secho("%*s" % (SIZE.DESC,"DESCRIPTION"), nl=False, bold=True)
+
+	# for idx,eachRepo in enumerate(repos):
+	# 	click.echo()
+	# 	click.secho("%*s" % (SIZE.IDX,str(idx+1)), nl=False, bold=True,fg=COLOR.IDX)
+	# 	click.secho("%*s" % (SIZE.NAME,eachRepo['NAME']), nl=False, bold=True,fg=COLOR.NAME)
+	# 	click.secho("%*s" % (SIZE.LANG,eachRepo['LANG']), nl=False, bold=True,fg=COLOR.LANG)
+	# 	click.secho("%*s" % (SIZE.STARS,eachRepo['STAR']), nl=False, bold=True,fg=COLOR.STARS)
+	# 	click.secho("%*s" % (SIZE.DESC,eachRepo['DESC']), bold=True,fg=COLOR.DESC)
 
 def write_console_dev(devs):
 	
