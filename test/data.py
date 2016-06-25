1d1fb79f740d5d19ee65f8275dd317b6d06dc91e
Taranjeet <reachtotj@gmail.com>
2016-06-25 12:32:22 +0530

    Test: Add Trending repo count in data

--- test/data.py
+++ test/data.py
@@ -1,6 +1,8 @@
 TRENDING_REPO_URL = 'http://github.com/trending'
 TRENDING_DEV_URL = 'http://github.com/trending/developers'
 
+TRENDING_REPO_COUNT = 25
+
 READ_PAGE_DATA = [
                     {
                         'url': TRENDING_REPO_URL,
