9e390096103c0827c1c097fedff48003e0efc9d9
Taranjeet <reachtotj@gmail.com>
2016-06-25 12:16:27 +0530

    Test: Add test for make_etree

--- test/test_github_trending.py
+++ test/test_github_trending.py
@@ -13,6 +13,16 @@ class TestGithubTrending(unittest.TestCase):
             response, status_code = githubtrending.read_page(url)
             self.assertEqual(status_code, expected_status_code)
 
+    def test_make_etree(self):
+        for each in data.READ_PAGE_DATA:
+            url = each.get('url')
+            expected_status_code = each.get('status_code')
+            expected_title = each.get('title').encode('utf8')
+            response, status_code = githubtrending.make_etree(url)
+            self.assertEqual(status_code, expected_status_code)
+            page_title = response.xpath('//title')[0].text.encode('utf8')
+            self.assertIn(expected_title, page_title)
+
 
 if __name__ == '__main__':
     unittest.main()
