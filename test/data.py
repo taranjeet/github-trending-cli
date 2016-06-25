e19f92825d302e1e26d8f7b7cdeb8e2379edf871
Taranjeet <reachtotj@gmail.com>
2016-06-25 12:16:43 +0530

    Test: Add data for make_etree

--- test/data.py
+++ test/data.py
@@ -2,9 +2,11 @@ READ_PAGE_DATA = [
                     {
                         'url': 'http://github.com/trending',
                         'status_code': 200,
+                        'title': 'repositories'
                     },
                     {
                         'url': 'http://github.com/trending/developers',
                         'status_code': 200,
+                        'title': 'developers'
                     },
                 ]
