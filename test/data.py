8ac67bcf84bb1be0c3d98151e779874ac40fea1a
Taranjeet <reachtotj@gmail.com>
2016-06-25 12:08:28 +0530

    Test: Add test data and package

---
+++ test/data.py
@@ -0,0 +1,10 @@
+READ_PAGE_DATA = [
+                    {
+                        'url': 'http://github.com/trending',
+                        'status_code': 200,
+                    },
+                    {
+                        'url': 'http://github.com/trending/developers',
+                        'status_code': 200,
+                    },
+                ]
