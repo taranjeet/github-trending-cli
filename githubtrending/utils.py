b268c86505b054d6101ddeea06d29e761877582d
Taranjeet <reachtotj@gmail.com>
2016-06-26 23:11:05 +0530

    Utils: Add get_col() and color()

---
+++ githubtrending/utils.py
@@ -0,0 +1,39 @@
+def get_print_size_for_repo(data):
+    name, lang, star = [0]*3
+    for each in data:
+        repo_name, desc, [stars, language] = each
+        name = max(len(repo_name), name)
+        lang = max(len(language), lang)
+        star = max(len(stars), star)
+
+    return {
+        "NAME": name+1,
+        "LANG": lang+1,
+        "STAR": star+1,
+        "IDX": 3,
+    }
+
+
+def get_print_size_for_dev(data):
+    dev, repo = [0]*2
+    for each in data:
+        dev_name, repo_name, description = each
+        dev = max(len(dev_name), dev)
+        repo = max(len(repo_name), repo)
+
+    return {
+        "DEV": dev+1,
+        "REPO": repo+1,
+        "IDX": 3,
+    }
+
+
+def get_color_code():
+    return {
+        "IDX": "white",
+        "NAME": "yellow",
+        "LANG": "red",
+        "STARS": "green",
+        "DESC": "blue",
+        "REPO": "green",
+    }
