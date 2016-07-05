92e72c53327e5eb887f17d437715369d43d0ce3c
Taranjeet <reachtotj@gmail.com>
2016-07-05 21:19:20 +0530

    Writers: Add print functions

---
+++ githubtrending/writers.py
@@ -0,0 +1,90 @@
+import click
+
+from . import utils
+
+def print_title(is_repo=True):
+    rows, cols = utils.get_console_size()
+    title = "TRENDING {} ON GITHUB".format("REPOSITORIES" if is_repo else "DEVELOPERS")
+    click.echo()
+    click.secho("%s" % ('#'*cols), bold=True)
+    click.secho("%*s" % (cols/2, title), bold=True)
+    click.secho("%s" % ('#'*cols), bold=True)
+
+def print_headers(print_size, is_repo=True):
+
+    print_title(is_repo)
+    click.echo()
+    
+    if is_repo:
+        click.secho(
+            "%*s" % (print_size["IDX"], '#'),
+            nl=False, bold=True)
+        click.secho(
+            "%*s" % (print_size["NAME"], "USER/REPO"),
+            nl=False, bold=True)
+        click.secho(
+            "%*s" % (print_size["LANG"], "LANG"),
+            nl=False, bold=True)
+        click.secho(
+            "%*s" % (print_size["STAR"], "STAR"),
+            nl=False, bold=True)
+        click.secho(
+            " %s" % ("DESCRIPTION"),
+            nl=False, bold=True)
+    else:
+        click.secho("%*s" % (print_size["IDX"], '#'), nl=False, bold=True)
+        click.secho("%*s" % (print_size["DEV"], "DEV"), nl=False, bold=True)
+        click.secho("%*s" % (print_size["REPO"], "REPO"), nl=False, bold=True)
+        click.secho(" %s" % ("DESCRIPTION"), nl=False, bold=True)
+
+
+def print_trending_repos(data):
+
+    print_size = utils.get_print_size_for_repo(data)
+    COLOR = utils.get_color_code()
+
+    print_headers(print_size)
+
+    for idx, each in enumerate(data):
+        repo_name, description, [stars, language] = each
+        click.echo()
+        click.secho(
+            "%*s" % (print_size["IDX"], str(idx+1)),
+            nl=False, bold=True, fg=COLOR['IDX'])
+        click.secho(
+            "%*s" % (print_size["NAME"], repo_name),
+            nl=False, bold=True, fg=COLOR['NAME'])
+        click.secho(
+            "%*s" % (print_size["LANG"], language),
+            nl=False, bold=True, fg=COLOR['LANG'])
+        click.secho(
+            "%*s" % (print_size["STAR"], stars),
+            nl=False, bold=True, fg=COLOR['STARS'])
+        click.secho(
+            " %s" % (description),
+            nl=False, bold=True, fg=COLOR['DESC'])
+    print('')
+
+def print_trending_devs(data):
+
+    print_size = utils.get_print_size_for_dev(data)
+    COLOR = utils.get_color_code()
+
+    print_headers(print_size, is_repo=False)
+
+    for idx, each in enumerate(data):
+        click.echo()
+        dev_name, repo_name, description = each
+        click.secho(
+            "%*s" % (print_size["IDX"], str(idx+1)),
+            nl=False, bold=True, fg=COLOR['IDX'])
+        click.secho(
+            "%*s" % (print_size["DEV"], dev_name),
+            nl=False, bold=True, fg=COLOR['NAME'])
+        click.secho(
+            "%*s" % (print_size["REPO"], repo_name),
+            nl=False, bold=True, fg=COLOR['REPO'])
+        click.secho(
+            " %s" % (description),
+            nl=False, bold=True, fg=COLOR['DESC'])
+    print('')
